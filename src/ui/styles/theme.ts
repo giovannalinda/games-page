23e4a1667df4511d0c9deb063007e09311d23a37
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-11-26 14:33:40 -0300

    adding config in the project

---
+++ src/ui/styles/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    white: '#FAFAFA',
+    black: '#020203',
+    blue: {
+      50: '#61CDFF',
+      100: '#00AEFF',
+      200: '#025A9A',
+      300: '#002550',
+    },
+  },
+  font: {
+    family: 'Poppins',
+    weigths: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+}
