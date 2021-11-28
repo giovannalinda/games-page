d262e56ce9e1867a530d01e30e42632da983f20e
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-11-28 00:03:51 -0300

    adding header and navbar

--- src/pages/Home/Home.styled.ts
+++ src/pages/Home/Home.styled.ts
@@ -1,2 +1,95 @@
 import styled from 'styled-components/macro'
 import { theme } from '../../ui'
+
+export const Container = styled.div`
+  margin: 0 auto;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: fixed;
+    background: ${theme.colors.black};
+    width: 100%;
+  }
+`
+
+export const Nav = styled.nav`
+  ul {
+    margin: -70px 0 38px 290px;
+  }
+
+  li {
+    display: inline-block;
+    text-align: left;
+    margin: 0 0 0 70px;
+  }
+
+  a {
+    color: ${theme.colors.white};
+    transition: 1s;
+
+
+    :hover {
+      color: ${theme.colors.blue[100]};
+    }
+  }
+
+  img {
+    display: flex;
+    width: 100px;
+    height: 100px;
+    margin-left: 190px;
+  }
+
+  button {
+    background: ${theme.colors.blue[100]};
+    border: 0;
+    color: ${theme.colors.white};
+    width: 115px;
+    height: 41px;
+    border-radius: 4px;
+    transition: 1s;
+    margin-left: 300px;
+
+    :hover {
+      opacity: 85%;
+    }
+  }
+`
+
+export const ContentHeader = styled.div`
+
+  img {
+    width: 100%;
+    height: 640px;
+    margin-top: 30px;
+  }
+
+  h1 {
+    margin-top: -450px;
+    padding: 0 190px;
+    font-size: 64px;
+    max-width: 970px;
+    line-height: 110.2%;
+  }
+
+  p {
+    padding: 16px 0 32px 190px;
+    font-size: 18px;
+  }
+
+  a {
+    background: ${theme.colors.blue[100]};
+    border: 0;
+    color: ${theme.colors.white};
+    padding: 15px 45px 15px 45px;
+    border-radius: 4px;
+    transition: 1s;
+    margin-left: 190px;
+
+    :hover {
+      opacity: 85%;
+    }
+  }
+`
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,32 @@
+import { Link } from 'react-router-dom'
+
+import image from '../../assets/logo-blizzard.svg'
+import bannerhome from '../../assets/header.png'
+
+import * as S from './Home.styled'
+
 export function Home () {
-  return <h1>Home</h1>
+  return (
+    <S.Container>
+      <header>
+        <S.Nav>
+          <img src={image} alt='logo' />
+          <ul>
+            <li><Link to=''>Jogos</Link></li>
+            <li><Link to=''>Esportes</Link></li>
+            <li><Link to=''>Loja</Link></li>
+            <li><Link to=''>Notícias</Link></li>
+            <li><Link to=''>Suporte</Link></li>
+            <button>Logar</button>
+          </ul>
+        </S.Nav>
+      </header>
+      <S.ContentHeader>
+        <img src={bannerhome} alt='banner' />
+        <h1>Retorne à escuridão com o game Diablo IV</h1>
+        <p>O retorno de Lilith traz uma era de escuridão e sofrimento</p>
+        <Link to=''>Jogue agora</Link>
+      </S.ContentHeader>
+    </S.Container>
+  )
 }
